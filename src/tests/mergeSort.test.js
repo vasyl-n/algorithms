71be6206dfef77536d29106d3be695139a1c0600
vasyl-n <vasyl.nrk@gmail.com>
2018-09-20 03:39:41 -0700

    adds tests as separate files

---
+++ src/tests/mergeSort.test.js
@@ -0,0 +1,26 @@
+const mergeSort = require('../sorting/mergeSort')
+const bubbleSort = require('../sorting/bubbleSort')
+
+const sorts = [mergeSort];
+sorts.forEach((sort) => {
+  test('sorts array', () => {
+    expect(sort([4,1,3,2])).toEqual([1,2,3,4]);
+  });
+  
+  test('sorts sorted array', () => {
+    expect(sort([1,2,3,4])).toEqual([1,2,3,4]);
+  });
+  
+  test('empty array', () => {
+    expect(sort([])).toEqual([]);
+  });
+  
+  test('one number array', () => {
+    expect(sort([5])).toEqual([5]);
+  });
+  
+  test('negative numbers', () => {
+    expect(sort([-45,23,-12,4,6,8,2,4,4,4])).toEqual([-45,-12,2,4,4,4,4,6,8,23]);
+  });
+})
+
