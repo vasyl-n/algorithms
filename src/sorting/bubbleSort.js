22d57ef1c79dc6d7f21529f148aa0f5cbcf0b4a8
vasyl-n <vasyl.nrk@gmail.com>
2018-09-20 03:45:38 -0700

    adds bubble sort

--- src/sorting/bubbleSort.js
+++ src/sorting/bubbleSort.js
@@ -1,6 +1,18 @@
-const bubbleSort = () => {
-
+const bubbleSort = (arr) => {
+  for ( let i = 0; i < arr.length; i++ ) {
+    for ( let j = 0; j < arr.length - 1; j++ ) {
+      if ( arr[j] > arr[j+1] ) {
+        swap(j, j + 1, arr);
+      }
+    }
+  }
+  return arr;
 }
 
+const swap = (a, b, arr) => {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
 
 module.exports = bubbleSort;
