1a24350fa81e378fa194444ac84c87f385f23278
vasyl-n <vasyl.nrk@gmail.com>
2018-09-22 14:03:20 -0700

    adds Linked list

---
+++ src/dataStructures/LinkedList.js
@@ -0,0 +1,24 @@
+const LinkedList = function() {
+  this.head = null;
+  this.tail = null;
+}
+
+const Node = function(value) {
+  this.value = value;
+  this.next = null;
+}
+
+LinkedList.prototype.addToTail = function(value) {
+  v
+}
+
+LinkedList.prototype.removeHead = function() {
+
+}
+
+LinkedList.prototype.contains = function(value) {
+
+}
+
+module.exports.LinkedList = LinkedList;
+module.exports.Node = Node;
---
+++ src/tests/linkedList.test.js
@@ -0,0 +1,39 @@
+const LinkedList = require('../dataStructures/LinkedList').LinkedList
+const Node = require('../dataStructures/LinkedList').Node
+
+test('linked list has all the methods', () => {
+  const list = new LinkedList();
+  expect( typeof list.addToTail === 'function').toBe(true);
+  expect( typeof list.removeHead === 'function').toBe(true);
+  expect( typeof list.contains === 'function').toBe(true)
+})
+
+test('one element is head and tail', () => { 
+  var list = new LinkedList();
+  list.addToTail(4);
+
+  expect(list.head.value).toEqual(4);
+  expect(list.tail.value).toEqual(4);
+})
+
+test('adds things to tail', () => {
+  var list = new LinkedList();
+  list.addToTail(4);
+  list.addToTail(5);
+  expect(list.head.value).toEqual(4);
+  expect(list.tail.value).toEqual(5);
+  list.addToTail(6)
+  expect(list.head.value).toEqual(4);
+  expect(list.tail.value).toEqual(6);
+})
+
+test('removes head', () => {
+  var list = new LinkedList();
+  list.addToTail(4);
+  list.addToTail(5);
+  expect(list.head.value).toEqual(4);
+  expect(list.tail.value).toEqual(5);
+  list.removeHead()
+  expect(list.head.value).toEqual(5);
+  expect(list.tail.value).toEqual(6);
+})
